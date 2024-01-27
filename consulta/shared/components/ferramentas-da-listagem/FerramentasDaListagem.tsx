136fce2de903b7e8c1c1e057a20cbefedc662ef4
Eliel S. <135076414+hydrahtec@users.noreply.github.com>
2024-01-27 16:10:40 -0300

    component ferramentasDaListagem

--- consulta/shared/components/ferramentas-da-listagem/FerramentasDaListagem.tsx
+++ consulta/shared/components/ferramentas-da-listagem/FerramentasDaListagem.tsx
@@ -1,1 +1,59 @@
-export {};
+import {Box, Button, Icon, Paper, TextFild, useTheme} from '@mui/material';
+
+import {Environment} from '../../environment';
+
+interface IFerrammentasDaListagemProps {
+    textoDaBusca?: string;
+    mostrarInputBusca?: boolean;
+    aoMudarTextoDeBusca?: (novoTexto: string) => void;
+    textoBotaoNovo?: string;
+    mostrarBotaoNovo?: boolean;
+    aoClicarEmNovo?: () => void;
+};
+
+export const FerramentasDaListagem: React.FC<IFerrammentasDaListagemProps> = ({
+    textoDaBusca='',
+    aoMudarTextoDeBusca,
+    mostrarInputBusca = false,
+    aoClicarEmNovo,
+    textoBotaoNovo = 'Novo',
+    mostrarBotaoNovo = true,
+}) => {
+    const theme = useTheme();
+
+    return (
+        <Box 
+            gap={1}
+            marginX={1}
+            padding={1}
+            paddingX={2}
+            display="flex"
+            alignItems="center"
+            height={theme.spacing(5)}
+            component={Paper}
+        >
+            {mostrarInputBusca && (
+                <TextFild 
+                size="small"
+                value={textoDaBusca}
+                placeholder={Environment.INPUT_DE_BUSCA}
+                onChange={(e) => aoMudarTextoDeBusca?.(e.target.value)}
+                />
+            )}
+
+            <Box flex={1} display="flex" justifyContent="end">
+                {mostrarBotaoNovo && (
+                    <Button 
+                        color='primary'
+                        disableElevation
+                        variant='contained'
+                        onClick={aoClicarEmNovo}
+                        endIcon={<Icon>add</Icon>}
+                    >
+                        {textoBotaoNovo}
+                    </Button>
+                )}
+            </Box>
+        </Box>
+    );
+};
