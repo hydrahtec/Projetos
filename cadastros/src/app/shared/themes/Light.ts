49779cb23a5d35e9ae1e778a2267555cc65119be
Eliel S <hydrahtec@gmail.com>
2024-01-28 13:32:56 -0300

    add feat thme light

--- cadastros/src/app/shared/themes/Light.ts
+++ cadastros/src/app/shared/themes/Light.ts
@@ -1,1 +1,23 @@
-export {};
+import { createTheme } from "@mui/material";
+import { cyan, yellow } from "@mui/material/colors";
+
+export const LightTheme = createTheme({
+    palette: {
+        primary: {//cor aplicada a botoes e lugares com mais destaque. main, dark,light, constast são variações.
+            main: yellow[700],
+            dark: yellow[800],
+            light: yellow[500],
+            contrastText: '#ffffff',
+        },
+        secondary: { //aplicada  a autocomplete
+            main: cyan[700],
+            dark: cyan[800],
+            light: cyan[500],
+            contrastText: '#ffffff',
+        },
+        background: {
+            paper: '#ffffff',
+            default: '#f7f6f3'
+        }
+    }
+});
