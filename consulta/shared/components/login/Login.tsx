477753eaf9f1e77b03819349ae4281438fd155fa
Eliel S <hydrahtec@gmail.com>
2024-01-27 23:01:04 -0300

    add component Login

--- consulta/shared/components/login/Login.tsx
+++ consulta/shared/components/login/Login.tsx
@@ -39,16 +39,67 @@ export const Login:React.FC<ILoginProps> = ({children}) => {
             setIsLoading(false);
 
             errors.inner.forEach(error => {
-                if () {
-
-                } else if () {
-
+                if (error.path === 'email') {
+                    setEmailError(error.message);
+                } else if (error.path === 'password') {
+                    setPasswordError(error.message)
                 }
             });
         });
     };
     
-    return (
+    if (isAuthenticated) return (
+        <>{children}</>
+    );
 
+    return (
+        <Box width='100vw' height='100vh' display='flex' alignItems='center' justifyContent='center'>
+
+            <Card>
+                <CardContent>
+                <Box display='flex' flexDirection='column' gap={2} width={250}>
+                    <Typography variant='h6' align='center'>Identifique-se</Typography>
+
+                    <TextField
+                    fullWidth
+                    type='email'
+                    label='Email'
+                    value={email}
+                    disabled={isLoading}
+                    error={!!emailError}
+                    helperText={emailError}
+                    onKeyDown={() => setEmailError('')}
+                    onChange={e => setEmail(e.target.value)}
+                    />
+
+                    <TextField
+                    fullWidth
+                    label='Senha'
+                    type='password'
+                    value={password}
+                    disabled={isLoading}
+                    error={!!passwordError}
+                    helperText={passwordError}
+                    onKeyDown={() => setPasswordError('')}
+                    onChange={e => setPassword(e.target.value)}
+                    />
+                </Box>
+                </CardContent>
+                <CardActions>
+                <Box width='100%' display='flex' justifyContent='center'>
+
+                    <Button
+                    variant='contained'
+                    disabled={isLoading}
+                    onClick={handleSubmit}
+                    endIcon={isLoading ? <CircularProgress variant='indeterminate' color='inherit' size={20} /> : undefined}
+                    >
+                    Entrar
+                    </Button>
+
+                </Box>
+                </CardActions>
+            </Card>
+        </Box>
     );
 };
