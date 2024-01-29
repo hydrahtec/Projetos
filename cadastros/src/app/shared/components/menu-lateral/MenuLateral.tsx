710c37bf2fe0b677e608e8c83d83ffe917abb236
Eliel S. <hydrahtec@gmail.com>
2024-01-29 09:39:23 -0300

    init cod MenuLateral

--- cadastros/src/app/shared/components/menu-lateral/MenuLateral.tsx
+++ cadastros/src/app/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,1 +1,15 @@
-export {};
+import { useTheme } from "@mui/material";
+
+
+interface IMenuLateralProps {
+    children: React.ReactNode;
+};
+
+export const MenuLateral: React.FC<IMenuLateralProps> = ({children}) => {
+    const theme = useTheme();
+
+
+    return(
+
+    );
+};
