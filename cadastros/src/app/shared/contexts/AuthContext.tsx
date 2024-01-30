99d00e1bcaf3fa0a61c66faa46036840b0a7fcf1
Eliel S. <135076414+hydrahtec@users.noreply.github.com>
2024-01-30 11:18:02 -0300

    feat: AuthContext + readme-install

--- cadastros/src/app/shared/contexts/AuthContext.tsx
+++ cadastros/src/app/shared/contexts/AuthContext.tsx
@@ -1,8 +1,58 @@
+import { createContext, useCallback, useContext, useEffect, useMemo, useState } from "react";
 
+import { AuthService } from '../services/api/auth/AuthService';
 
-export const AuthContext = () => {
+interface IAuthContextData {
+    logout: () => void;
+    isAuthenticated: boolean;
+    login: (email: string, password: string) => Promise<string | void>;
+};
+
+const AuthContext = createContext({} as IAuthContextData);
+
+const LOCAL_STORAGE_KEY_ACESS_TOKEN = 'APP_ACCESS_TOKEN';
+
+interface IAuthProviderProps {
+    children: React.ReactNode;
+}
+
+export const AuthProvider: React.FC<IAuthProviderProps> = ({children}) => {
+    const [accessToken, setAccessToken] = useState<string>()
+
+    useEffect(() => {
+        const acessToken = localStorage.getItem(LOCAL_STORAGE_KEY_ACESS_TOKEN);
+
+        if (acessToken) {
+            setAccessToken(JSON.parse(acessToken));
+        } else {
+            setAccessToken(undefined);
+        }
+    }, []);
+
+    const handleLogin = useCallback(async (email: string, password: string) => {
+        const result = await AuthService.auth(email, password);
+
+        if (result instanceof Error) {
+            return result.message;
+        } else {
+            localStorage.setItem(LOCAL_STORAGE_KEY_ACESS_TOKEN, JSON.stringify(result.accessToken));
+
+            setAccessToken(result.accessToken);
+        }
+    }, []);
+
+    const handleLogout = useCallback(() => {
+        localStorage.removeItem(LOCAL_STORAGE_KEY_ACESS_TOKEN);
+        setAccessToken(undefined);
+    }, []);
+
+    const isAuthenticated = useMemo(() => !!accessToken, [accessToken]);
 
     return (
-        <p>AuthContext</p>
+        <AuthContext.Provider value={{isAuthenticated, login: handleLogin, logout: handleLogout}}>
+            {children}
+        </AuthContext.Provider>
     );
 };
+
+export const useAuthContext = () => useContext(AuthContext);
