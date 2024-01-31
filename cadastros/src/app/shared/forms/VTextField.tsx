8e5794a3e5253112209493fb00fa0730c40eac20
Eliel Silva <hydrahtec@gmail.com>
2024-01-30 23:00:40 -0300

    feat: VTextField

--- cadastros/src/app/shared/forms/VTextField.tsx
+++ cadastros/src/app/shared/forms/VTextField.tsx
@@ -1,1 +1,35 @@
-export {};
+import React, {useEffect, useState} from 'react';
+import { TextField, TextFieldProps } from '@mui/material'; 
+import { useField } from '@unform/core';
+
+type TVTextFieldProps = TextFieldProps & {
+    name: string;
+};
+
+export const VTextField: React.FC<TVTextFieldProps> = ({name, ...rest}) => {
+    const {fieldName, defaultValue, registerField, error, clearError} = useField(name);
+
+    const [value, setValue] = useState(defaultValue || '');
+    
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            getValue: () => value,
+            setValue: (_, value) => setValue(value),
+        });
+    }, [fieldName, value, registerField]);
+
+    return (
+        <TextField
+            {...rest}
+
+            defaultValue={defaultValue}
+            value={value || ''}
+            error={!!error}
+            helperText={error}
+            
+            onKeyDown = {e => {error && clearError(); rest.onKeyDown?.(e);}}
+            onChange = {e => {setValue(e.target.value); rest.onChange?.(e);}}
+        />
+    );
+};
