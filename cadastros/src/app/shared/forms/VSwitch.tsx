bdb123916ba28ef3464d9195c7fcaf06326c567e
Eliel Silva <hydrahtec@gmail.com>
2024-01-30 22:55:11 -0300

    feat: VSwitch

--- cadastros/src/app/shared/forms/VSwitch.tsx
+++ cadastros/src/app/shared/forms/VSwitch.tsx
@@ -1,1 +1,32 @@
-export {};
+import React, {useEffect, useState} from 'react';
+import { Switch, SwitchProps } from '@mui/material'; 
+import { useField } from '@unform/core';
+
+type TVSwitchProps = SwitchProps & {
+    name: string;
+};
+
+export const VSwitch: React.FC<TVSwitchProps> = ({name, ...rest}) => {
+    const {fieldName, defaultValue, registerField, error, clearError} = useField(name);
+
+    const [value, setValue] = useState(defaultValue || false);
+    
+    useEffect(() => {
+        registerField({
+            name: fieldName,
+            getValue: () => value,
+            setValue: (_, value) => setValue(value),
+        });
+    }, [fieldName, value, registerField]);
+
+    return (
+        <Switch
+            {...rest}
+
+            defaultChecked={defaultValue}
+
+            checked={value || false}
+            onChange={(e, checked) => {setValue(checked); rest.onChange?.(e, checked); error && clearError();}}
+        />
+    );
+};
