ce92b8d44d1ee4b05fafb5e9b561de567a9837ec
Eliel S. <135076414+hydrahtec@users.noreply.github.com>
2024-02-01 11:44:45 -0300

    feat: ListagemDeCidades componente finalizado

--- cadastros/src/app/pages/cidades/ListagemDeCidades.tsx
+++ cadastros/src/app/pages/cidades/ListagemDeCidades.tsx
@@ -62,6 +62,73 @@ export const ListagemDeCidades: React.FC= () => {
     };
 
     return(
-        
+        <LayoutBaseDePagina 
+            titulo='Listagem de cidades'
+            barraDeFerramentas={
+                <FerramentasDaListagem 
+                    mostrarInputBusca
+                    textoDaBusca={busca}
+                    textoBotaoNovo='Nova'
+                    aoClicarEmNovo={() => navigate('/cidades/detalhe/nova')}
+                    aoMudarTextoDeBusca={texto => setSearchParams({busca: texto, pagina: '1'}, {replace: true})}
+                />
+            }
+        >
+            <TableContainer component={Paper} variant="outlined" sx={{m: 1, width: 'auto'}}>
+
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell width={100}>Ações</TableCell>
+                            <TableCell>Nome</TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {rows.map(row => (
+                           <TableRow key={row.id}>
+                            <TableCell>
+                                <IconButton size="small" onClick={() => handleDelete(row.id)}>
+                                    <Icon>delete</Icon>
+                                </IconButton>
+                                <IconButton size="small" onClick={() => navigate(`/cidades/detalhe/${row.id}`)}>
+                                    <Icon>edit</Icon>
+                                </IconButton>
+                            </TableCell>
+
+                            <TableCell>{row.nome}</TableCell>
+
+                           </TableRow> 
+                        ))}
+                    </TableBody>
+
+                    {totalCount === 0 && !isLoading && (
+                        <caption>{Environment.LISTAGEM_VAZIA}</caption>
+                    )}
+
+                    <TableFooter>
+                    {isLoading && (
+                        <TableRow>
+                            <TableCell colSpan={3}>
+                                <LinearProgress variant='indeterminate' />
+                            </TableCell>
+                        </TableRow>
+                    )}
+
+                    {(totalCount > 0 && totalCount > Environment.LIMITE_DE_LINHAS) && (
+                        <TableRow>
+                            <TableCell colSpan={3}>
+                                <Pagination 
+                                    page={pagina}
+                                    count={Math.ceil(totalCount / Environment.LIMITE_DE_LINHAS)}
+                                    onChange={(_, newPage) => setSearchParams({busca, pagina: newPage.toString()}, {replace: true})}
+                                />
+                            </TableCell>
+                        </TableRow>
+                    )}
+                    </TableFooter>
+                </Table>
+            </TableContainer>
+        </LayoutBaseDePagina>
     );
 };
