f6d721627695a9dc6926fb6b752403bbd377d169
Eliel S. <135076414+hydrahtec@users.noreply.github.com>
2024-02-12 11:34:34 -0300

    component Movie v.1

--- film_flix/src/pages/Movie.jsx
+++ film_flix/src/pages/Movie.jsx
@@ -1,10 +1,21 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {useParams} from 'react-router-dom';
-import {BsGraphUp} from ''
+import {BsGraphUp} from 'react-icons/bs';
 
+import MovieCard from  '../components/MovieCard';
+
+import './css/movie.css';
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 export const Movie = () => {
+    const {id} = useParams();
+    cosnt [moviesURL, setMovie] = useState(null);
     
+    const getMovie = async (url) => {
+        
+    };
     
     return(
         teste
