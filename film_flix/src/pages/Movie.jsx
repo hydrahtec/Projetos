f9ea45faabec206394462b5922e2381255a69f09
Eliel S. <135076414+hydrahtec@users.noreply.github.com>
2024-02-12 12:46:57 -0300

    component Movie finesh

--- film_flix/src/pages/Movie.jsx
+++ film_flix/src/pages/Movie.jsx
@@ -37,7 +37,33 @@ export const Movie = () => {
        <div classname='movie-page'>
         {Movie && (
             <>
-                
+                <MovieCard movie={movie} showLink={false} />
+                <p classname='tagline'>{movie.tagline}</p>
+
+                <div className='info'>
+                    <h3>
+                        <BsWallet2 /> Orçamento:
+                    </h3>
+                    <p>{formatCurrency(movie.budget)}</p>
+                </div>
+                <div className='info'>
+                    <h3>
+                        <BsGraphUp /> Receita:
+                    </h3>
+                    <p>{formatCurrency(movie.revenue)}</p>
+                </div>
+                <div className='info'>
+                    <h3>
+                        <BsHourglassSplit /> Duração:
+                    </h3>
+                    <p>{movie.runtime} minutos</p>
+                </div>
+                <div className='info description'>
+                    <h3>
+                        <BsFillFileEarmarkTextFill /> Descrição:
+                    </h3>
+                    <p>{movie.overview}</p>
+                </div>
             </>
         )}
        </div> 
