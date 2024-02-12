9074f78675368dbd2ace45b10409b9083cb8c512
Eliel S. <135076414+hydrahtec@users.noreply.github.com>
2024-02-12 10:40:27 -0300

    component NavBar

--- film_flix/src/components/Navbar.jsx
+++ film_flix/src/components/Navbar.jsx
@@ -1,1 +1,42 @@
-export {};
+import { useState } from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi'
+
+import './css/navbar.css';
+
+export const Navbar = () => {
+    const [search, setSearch] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!search) return;
+
+        navigate(`/search?q=${search}`, {replace: true});
+        setSearch('');
+    };
+
+    return(
+       <nav className='navbar'>
+        <h2>
+            <Link to='/'>
+                <BiCameraMovie /> MoviesLib
+            </Link>
+        </h2>
+
+        <form onSubmit={handleSubmit}>
+            <input
+                type='text'
+                placeholder='Busque um filme'
+                onChange={(e) => setSearch(e.target.value)}
+                value={search}
+            />
+
+            <button type='submit'>
+                <BiSearchAlt2 />
+            </button>
+        </form>
+       </nav> 
+    );
+};
