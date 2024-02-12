418e3204073608a7d7d126c793a738a29acf05ab
Eliel S. <135076414+hydrahtec@users.noreply.github.com>
2024-02-12 10:02:17 -0300

    component MovieCard

--- film_flix/src/components/MovieCard.jsx
+++ film_flix/src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
-export {};
+import {Link} from 'react-router-dom';
+
+import {FaStar} from 'react-icons/fa';
+
+const imagesURL = import.meta.env.VITE_IMG;
+
+export const MovieCard = () => {
+    return (
+        <div className='movie-card'>
+            <img src={imagesURL + MovieCard.poster_path} alt={MovieCard.title} />
+
+            <h2>{MovieCard.title}</h2>
+
+            <p>
+               <FaStar /> {MovieCard.vote_average} 
+            </p>
+
+            {showLink && <Link to={`/movie/${movie.id}`}>Detalhes</Link>}
+        </div>
+    );
+};
