d275da86ef7c7350b76853f501890f397de4bd8a
Eliel S. <135076414+hydrahtec@users.noreply.github.com>
2024-02-12 11:47:03 -0300

    component Movie v.2

--- film_flix/src/pages/Movie.jsx
+++ film_flix/src/pages/Movie.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import {useParams} from 'react-router-dom';
-import {BsGraphUp} from 'react-icons/bs';
+import {BsGraphUp, BsWallet2, BsHourglassSplit, BsFillFileEarmarkTextFill} from 'react-icons/bs';
 
 import MovieCard from  '../components/MovieCard';
 
@@ -14,10 +14,32 @@ export const Movie = () => {
     cosnt [moviesURL, setMovie] = useState(null);
     
     const getMovie = async (url) => {
-        
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data);
+        setMovie(data);
     };
     
+    const formatCurrency = (number) => {
+        return number.toLocaleString('en-US', {
+            style: 'currency',
+            currency: 'USD'
+        });
+    };
+
+    useEffect(() => {
+        const movieUrl = `${moviesURL}${id}?${apiKey}`;
+        getMovie(movieUrl);
+    }, []);
+
+
     return(
-        teste
+       <div classname='movie-page'>
+        {Movie && (
+            <>
+                
+            </>
+        )}
+       </div> 
     );
 };
