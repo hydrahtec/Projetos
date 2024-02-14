d01f89402fd7568c7d444d60822905b8a9d0dfdc
Eliel S. <135076414+hydrahtec@users.noreply.github.com>
2024-02-14 14:10:33 -0300

    config data api

--- film_flix/src/environment/Environment.js
+++ film_flix/src/environment/Environment.js
@@ -1,6 +1,6 @@
 export const Environment = {
-    API_KEY: teste,
-    API_URL: teste,
-    API_SEARCH: teste,
-    API_IMG: teste,
+    API_KEY: 'api_key=70662fd4135b92fe0fc87874e13356c4',
+    API_URL: 'https://api.themoviedb.org/3/movie/',
+    API_SEARCH: 'https://api.themoviedb.org/3/search/movie',
+    API_IMG: 'https://image.tmdb.org/t/p/w500/',
 };
