4a1e1aa0c9844fdcae32bb4f58295a18fb4220e9
Eliel S. <135076414+hydrahtec@users.noreply.github.com>
2024-02-14 15:32:15 -0300

    ajustes css

--- film_flix/src/components/Navbar.jsx
+++ film_flix/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
-import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi'
+import {BiCameraMovie, BiSearchAlt2} from 'react-icons/bi';
 
 import './css/navbar.css';
 
@@ -18,25 +18,25 @@ export const Navbar = () => {
     };
 
     return(
-       <nav className='navbar'>
-        <h2>
-            <Link to='/'>
-                <BiCameraMovie /> MoviesLib
-            </Link>
-        </h2>
+        <nav id='navbar'>
+            <h2>
+                <Link to='/'>
+                    <BiCameraMovie /> Film Flix
+                </Link>
+            </h2>
 
-        <form onSubmit={handleSubmit}>
-            <input
-                type='text'
-                placeholder='Busque um filme'
-                onChange={(e) => setSearch(e.target.value)}
-                value={search}
-            />
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    placeholder='Busque um filme'
+                    onChange={(e) => setSearch(e.target.value)}
+                    value={search}
+                />
 
-            <button type='submit'>
-                <BiSearchAlt2 />
-            </button>
-        </form>
-       </nav> 
+                <button type='submit'>
+                    <BiSearchAlt2 />
+                </button>
+            </form>
+        </nav> 
     );
 };
