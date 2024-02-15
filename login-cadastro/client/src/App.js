9b081fb878e648a6cc329640f0be449a559b5342
Eliel S. <135076414+hydrahtec@users.noreply.github.com>
2024-02-15 10:53:49 -0300

    const + handles

--- login-cadastro/client/src/App.js
+++ login-cadastro/client/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import * as yup from 'yup';
+import Axios from 'axios';
+import {ErrorMessage, Formik, Form, Field} from 'formik';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
+    const handleLogin = (values) => {
+        Axios.post("http://localhost:3001/login", {
+            email: values.email,
+            password: values.password,
+        })
+        .then((response) => {
+            alert(response.data.msg)
+        });
+    };
 
-export default App;
+    const handleRegister = (values) => {
+        Axios.post("http://localhost:3001/register", {
+          email: values.email,
+          password: values.password,
+        }).then((response) => {
+          alert(response.data.msg);
+          console.log(response);
+        });
+    };
+    
+    const validationsLogin = yup.object().shape({
+        email: yup
+            .string()
+            .email('email inválido')
+            .required('O email é obrigatório'),
+        password: yup
+            .string()
+            .min(8,'A senha deve ter pelo menos 8 caracteres')
+            .required('A senha é obrigatória'),
+    });
+    
+    const validationsRegister = yup.object().shape({
+        email: yup
+            .string()
+            .email('email inválido')
+            .required('O email é obrigatório'),
+        password: yup
+            .string()
+            .min(8, 'A senha deve ter peli menos 8 caracteres')
+            .required('A senha é obrigatória'),
+        confirmation: yup
+            .string()
+            .oneOf([yup.ref('password'), null], 'As senhas são diferentes')
+            .required('A confirmação da senha é obrigatória'),
+    });
+    
+    
+
+
+    return (
+        <div>
+        </div>
+    );
+};
