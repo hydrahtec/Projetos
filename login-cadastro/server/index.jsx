9911b978e7db47c531ef0e6d6d54eba6b62835d7
Eliel S. <135076414+hydrahtec@users.noreply.github.com>
2024-02-16 09:33:37 -0300

    server index register

--- login-cadastro/server/index.jsx
+++ login-cadastro/server/index.jsx
@@ -15,4 +15,33 @@ const db = mysql.createPool({
 app.use(express.json());
 app.use(cors());
 
-app.post()
+app.post('/register', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    db.query('SELECT * FROM usuarios WHERE email = ?', [email], (err, result) => {
+        if (err) {
+            res.send(err);
+        }
+        if (result.length == 0) {
+            bcrypt.hash(password, saltRounds, (err, hash) => {
+                db.query(
+                    'INSERT INTO usuarios (email, password) VALUE (?,?)', [email, hash], (error, response) => {
+                        if (err) {
+                            res.send(err);
+                        }
+                        res.send({msg: 'Usuário cadastrado com sucesso'});
+                    }
+                );
+            });
+        } else {
+            res.send({msg: 'Email já cadastrado'});
+        }
+    });
+});
+
+app.post();
+
+app.listen(3001, () => {
+    console.log('rodando na porta 3001');
+});
