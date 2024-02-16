2aac94e944cb281dbe9d36117ac54ff277931c21
Eliel S. <135076414+hydrahtec@users.noreply.github.com>
2024-02-16 12:24:40 -0300

    server index login

--- login-cadastro/server/index.jsx
+++ login-cadastro/server/index.jsx
@@ -39,8 +39,30 @@ app.post('/register', (req, res) => {
         }
     });
 });
+app.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
-app.post();
+    db.query('SELECT * FROM usuarios WHERE email = ?', [email], (err, result) => {
+        if (err) {
+            res.send(err);
+        }
+        if (result.length > 0) {
+            bcrypt.compare(password, result[0].password, (error, response) => {
+                if (error) {
+                    res.send(error);
+                }
+                if (response) {
+                    res.send({msg: 'Usuário logado'});
+                } else {
+                    res.send({msg: 'Senha incorreta'});
+                }
+            });
+        } else {
+            res.send({msg: 'Usuário não registrado'});
+        }
+    });
+});
 
 app.listen(3001, () => {
     console.log('rodando na porta 3001');
