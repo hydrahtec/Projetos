cdc3869038d9f8c33034f05581d837a976a00786
Eliel S. <135076414+hydrahtec@users.noreply.github.com>
2024-02-17 16:00:20 -0300

    index files

--- painel-administrativo/src/components/index.js
+++ painel-administrativo/src/components/index.js
@@ -1,1 +1,7 @@
-export {};
+export * from './GeographyChart';
+export * from './ProgressCircle';
+export * from './LineChart';
+export * from './PieChart';
+export * from './BarChart';
+export * from './StatBox';
+export * from './Header';
--- painel-administrativo/src/data/index.js
+++ painel-administrativo/src/data/index.js
@@ -1,1 +1,2 @@
-export {};
+export * from './mockData';
+export * from './mockGeoFeatures';
