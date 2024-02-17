debde0bd1c40565486b2f6312298fa62f19cad72
Eliel Silva <hydrahtec@gmail.com>
2024-02-17 17:42:35 -0300

    barchar

--- painel-administrativo/src/components/BarChart/BarChart.jsx
+++ painel-administrativo/src/components/BarChart/BarChart.jsx
@@ -1,1 +1,9 @@
-export {};
+import {useTheme} from '@mui/material';
+import { ResponsiveBar } from '@nivo/bar';
+import {tokens} from '';
+import {mockBarData as data} from '';
+
+export const BarChart = ({isDashboard = false}) => {
+    const theme = useTheme();
+
+};
