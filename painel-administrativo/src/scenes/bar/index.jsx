89ca4620582fb0d0fabbacd468853f989175bb92
Eliel Silva <hydrahtec@gmail.com>
2024-02-18 14:42:19 -0300

    bar

--- painel-administrativo/src/scenes/bar/index.jsx
+++ painel-administrativo/src/scenes/bar/index.jsx
@@ -1,1 +1,13 @@
-export {}
+import {Box} from '@mui/material';
+import {Header, BarChart} from '../../components';
+
+export const Bar = () => {
+    return (
+        <Box m='20px'>
+            <Header title='Bar Chart' subtitle='Simple Bar Chart' />
+            <Box height='75vh'>
+                <BarChart />
+            </Box>
+        </Box>
+    );
+};
