3714f473a313d152ad9ccb6027d93e6a4b310108
Eliel Silva <hydrahtec@gmail.com>
2024-06-26 19:01:43 -0300

    docs: react cinemania navbar estrutura

---
+++ cinemania/src/components/Input/index.tsx
@@ -0,0 +1,10 @@
+//import style form './style.min.module.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+
+    </footer>
+  );
+};
+export default Footer;
--- cinemania/src/components/NavBar/index.tsx
+++ cinemania/src/components/NavBar/index.tsx
@@ -1,6 +1,55 @@
-//import style form './style.min.module.css';
+import Link from 'next/link';
+import style from './style.min.module.css';
+import { BiArrowFromTop, BiSearchAlt, BiSolidCameraMovie } from 'react-icons/bi';
+//import Input from '@/components/Input';
 
 const NavBar = () => {
-  <nav className="navbar"></nav>;
+  return (
+    <nav className={style.navbar}>
+      <div id="logo">
+        <BiSolidCameraMovie />
+        <h2 className="logo_title">Cinemania</h2>
+      </div>
+      <ul id="menu">
+        <li>Navegar</li>
+        <li>
+          <Link href={'/'}>Inicio</Link>
+        </li>
+        <li>
+          <Link href={'/'}>Filmes</Link>
+        </li>
+        <li>
+          <Link href={'/'}>Series</Link>
+        </li>
+        <li>
+          <Link href={'/'}>Bombando</Link>
+        </li>
+        <li className="genro">
+          <p>
+            Gênero <BiArrowFromTop />
+          </p>
+          <ul>
+            <li>genero</li>
+            <li>genero</li>
+            <li>genero</li>
+            <li>genero</li>
+            <li>genero</li>
+            <li>genero</li>
+            <li>genero</li>
+            <li>genero</li>
+            <li>genero</li>
+            <li>genero</li>
+            <li>genero</li>
+          </ul>
+        </li>
+      </ul>
+      <form id="search">
+        <label htmlFor="search">
+          <BiSearchAlt />
+        </label>
+        <input type="text" placeholder="Buscar" />
+      </form>
+    </nav>
+  );
 };
 export default NavBar;
