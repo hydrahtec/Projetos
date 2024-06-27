fc789e713fbf0d6fdfea3c8920646012dedacc8f
Eliel Silva <hydrahtec@gmail.com>
2024-06-27 18:32:40 -0300

    docs: react cinemania footer

--- cinemania/src/components/Footer/index.tsx
+++ cinemania/src/components/Footer/index.tsx
@@ -1,9 +1,14 @@
-//import style form './style.min.module.css';
+import Link from 'next/link';
+import style from './style.min.module.css';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-
+    <footer className={style.footer}>
+      <p>
+        <Link href="/">Politica de privacidade</Link> -{' '}
+        <Link href="/">Termos de uso</Link>
+      </p>
+      <p>Desenvolvido por Hydrah Technologia - todos os direitos reservados</p>
     </footer>
   );
 };
