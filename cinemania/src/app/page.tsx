7bb3d8eb48c802124b653f36861be864ae948092
Eliel Silva <hydrahtec@gmail.com>
2024-06-27 22:34:07 -0300

    docs: react cinemania section top_rated v2

--- cinemania/src/app/page.tsx
+++ cinemania/src/app/page.tsx
@@ -21,12 +21,38 @@ export default function Home() {
       </section>
 
       <section className={styles.top_rated}>
-        <h3>Filmes mais procurados:</h3>
-        <div className="movie_card">
-          <img src="https://picsum.photos/150/200" alt="imagem do filme" />
-          <h2>Titulo do filme</h2>
-          <p>votos e views</p>
-          <Link href="/">Detalhes</Link>
+        <h3 className={styles.title_sec}>Filmes mais procurados:</h3>
+        <div className={styles.movie_container}>
+          <div className={styles.movie_card}>
+            <img src="https://picsum.photos/200/250" alt="imagem do filme" />
+            <h2>Titulo do filme</h2>
+            <p>votos e views</p>
+            <Link href="/">Detalhes</Link>
+          </div>
+          <div className={styles.movie_card}>
+            <img src="https://picsum.photos/150/200" alt="imagem do filme" />
+            <h2>Titulo do filme</h2>
+            <p>votos e views</p>
+            <Link href="/">Detalhes</Link>
+          </div>
+          <div className={styles.movie_card}>
+            <img src="https://picsum.photos/150/200" alt="imagem do filme" />
+            <h2>Titulo do filme</h2>
+            <p>votos e views</p>
+            <Link href="/">Detalhes</Link>
+          </div>
+          <div className={styles.movie_card}>
+            <img src="https://picsum.photos/150/200" alt="imagem do filme" />
+            <h2>Titulo do filme</h2>
+            <p>votos e views</p>
+            <Link href="/">Detalhes</Link>
+          </div>
+          <div className={styles.movie_card}>
+            <img src="https://picsum.photos/150/200" alt="imagem do filme" />
+            <h2>Titulo do filme</h2>
+            <p>votos e views</p>
+            <Link href="/">Detalhes</Link>
+          </div>
         </div>
       </section>
     </main>
