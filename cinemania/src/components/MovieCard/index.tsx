f70a8e238c14d48dc9817c3a312caf4cd9e61dd7
Eliel Silva <hydrahtec@gmail.com>
2024-06-28 20:23:42 -0300

    docs: react cinemania movie card props

--- cinemania/src/components/MovieCard/index.tsx
+++ cinemania/src/components/MovieCard/index.tsx
@@ -2,32 +2,32 @@
 
 import styles from './style.min.module.css';
 import Link from 'next/link';
+import { tmdb } from '@/api/tmdbApi';
+import { FaStar } from 'react-icons/fa';
 
 interface TCard {
-  movie: object[]
+  movie: object;
   showMovie?: boolean;
-};
+}
 
-const MovieCard = ({movie, showMovie = true}: TCard) => {
-
+const MovieCard = ({ movie, showMovie = true }: TCard) => {
 
   return (
     <div className={styles.movie_card}>
-            <img src="https://picsum.photos/200/250" alt="imagem do filme" />
-            <h2>Titulo do filme</h2>
-            <p>votos e views</p>
-            <div className={styles.card_overview}>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero,
-                amet. Voluptatibus, dignissimos corrupti tempora error dolor
-                sint est maiores quia molestiae reiciendis facere,
-                reprehenderit, distinctio a sit quibusdam voluptatem
-                repudiandae?
-              </p>
-              <Link href="/">Detalhes</Link>
-            </div>
-          </div>
-  )
+      <img src={tmdb.API_IMG + movie.poster_path} alt={movie.title} />
+      <h2>{movie.title}</h2>
+      <p>
+        <FaStar />
+        {movie.vote_average.toFixed(2)} - {`Votos: ${movie.vote_count}`}
+      </p>
+      <div className={styles.card_overview}>
+        <p>
+          {movie.overview}
+        </p>
+        <Link href="/">Detalhes</Link>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
