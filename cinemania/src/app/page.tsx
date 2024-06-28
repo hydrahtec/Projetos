685d343a08b0570e00617f00e0ae81810f1eded0
Eliel Silva <hydrahtec@gmail.com>
2024-06-27 21:47:32 -0300

    docs: react cinemania home section header

--- cinemania/src/app/page.tsx
+++ cinemania/src/app/page.tsx
@@ -4,7 +4,17 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <section className={styles.header}>
-      
+        <div>
+          <h2>Titulo do filme</h2>
+          <p className={styles.clasification}>Quantidade de estrelas</p>
+          <p className={styles.decript}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi non
+            assumenda maxime vero. A in, sequi quis, fugit dolorum optio,
+            reprehenderit natus nesciunt odit doloremque obcaecati quos?
+            Distinctio, nobis doloribus.
+          </p>
+          <button className={styles.header_info}><a href="#">Mais informações</a></button>
+        </div>
       </section>
     </main>
   );
