6858f2843620d40594cde2ad252f15fcb6922546
Eliel Silva <hydrahtec@gmail.com>
2024-06-28 23:07:09 -0300

    docs: react cinemania header fundo v1

--- cinemania/src/app/page.tsx
+++ cinemania/src/app/page.tsx
@@ -1,39 +1,57 @@
-'use client'
+'use client';
 import { useEffect, useState } from 'react';
 import styles from './page.module.css';
 import MovieCard from '@/components/MovieCard';
 import { tmdb } from '@/api/tmdbApi';
+import { FaStar } from 'react-icons/fa';
 
 const TMBD = tmdb;
 
 export default function Home() {
   const [topMovies, setTopMovies] = useState([]);
+  const [priMv, setPrimv] = useState({});
 
   const getMovies = async (url: string) => {
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.results);
+
     setTopMovies(data.results);
+
+    setPrimv(data.results[0]);
+  };
+
+  const mudaFundo = (urlImg: string) => {
+    const headerImg = document.getElementById('header') as HTMLElement;
+
+    headerImg.style.backgroundImage = `url('${urlImg}')`;
   };
 
   useEffect(() => {
     const topRated = TMBD.API_TOP_RATED;
 
     getMovies(topRated);
-  }, []);
+
+    if (!priMv) {
+      return;
+    } else {
+      const urlposter = priMv.poster_path;
+
+      if (urlposter) {
+        const imgH = TMBD.API_IMG + urlposter;
+        mudaFundo(imgH);
+      }
+    }
+  }, [topMovies, priMv]);
 
   return (
     <main className={styles.main}>
-      <section className={styles.header}>
+      <section id="header" className={styles.header}>
         <div>
-          <h2>Titulo do filme</h2>
-          <p className={styles.clasification}>Quantidade de estrelas</p>
-          <p className={styles.decript}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi non
-            assumenda maxime vero. A in, sequi quis, fugit dolorum optio,
-            reprehenderit natus nesciunt odit doloremque obcaecati quos?
-            Distinctio, nobis doloribus.
+          <h2>{priMv.title}</h2>
+          <p className={styles.clasification}>
+            {priMv.vote_average} <FaStar /> - {`Votos: ${priMv.vote_count}`}
           </p>
+          <p className={styles.decript}>{priMv.overview}</p>
           <button className={styles.header_info}>
             <a href="#">Mais informações</a>
           </button>
@@ -43,7 +61,9 @@ export default function Home() {
       <section className={styles.top_rated}>
         <h3 className={styles.title_sec}>Filmes mais procurados:</h3>
         <div className={styles.movie_container}>
-          {topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+          {topMovies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
         </div>
       </section>
     </main>
