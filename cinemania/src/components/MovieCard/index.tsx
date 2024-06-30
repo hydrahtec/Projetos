ca4b8b801f8b020204637ec76a0c555cb095955f
Eliel Silva <hydrahtec@gmail.com>
2024-06-30 19:00:28 -0300

    docs: react cinemania MovieCard ajuste de layout

--- cinemania/src/components/MovieCard/index.tsx
+++ cinemania/src/components/MovieCard/index.tsx
@@ -7,23 +7,19 @@ import { FaStar } from 'react-icons/fa';
 
 interface TCard {
   movie: object;
-  showMovie?: boolean;
 }
 
-const MovieCard = ({ movie, showMovie = true }: TCard) => {
-
+const MovieCard = ({ movie }: TCard) => {
   return (
     <div className={styles.movie_card}>
       <img src={tmdb.API_IMG + movie.poster_path} alt={movie.title} />
-      <h2>{movie.title}</h2>
       <p>
         <FaStar />
         {movie.vote_average.toFixed(2)} - {`Votos: ${movie.vote_count}`}
       </p>
       <div className={styles.card_overview}>
-        <p>
-          {movie.overview}
-        </p>
+        <h2>{movie.title}</h2>
+        <p>{movie.overview}</p>
         <Link href="/">Detalhes</Link>
       </div>
     </div>
