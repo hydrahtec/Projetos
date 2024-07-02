eb7eeef0432618c155aca2923ca05c3bf863cd31
Eliel Silva <hydrahtec@gmail.com>
2024-07-01 23:07:30 -0300

    docs: react portifolio estrutura home

--- portifolio/src/app/page.tsx
+++ portifolio/src/app/page.tsx
@@ -1,8 +1,21 @@
 import styles from './page.module.css';
 
 export default function Home() {
-  return;
-  <main className={styles.main}>
-    <h1>Portifolio</h1>
-  </main>;
+  return (
+    <main className={styles.main}>
+      <h1>Portifolio</h1>
+      <h1>Portifolio</h1>
+      <h1>Portifolio</h1>
+      <h1>Portifolio</h1>
+      <h1>Portifolio</h1>
+      <h1>Portifolio</h1>
+      <h1>Portifolio</h1>
+      <h1>Portifolio</h1>
+      <h1>Portifolio</h1>
+      <h1>Portifolio</h1>
+      <h1>Portifolio</h1>
+      <h1>Portifolio</h1>
+      <h1>Portifolio</h1>
+    </main>
+  );
 }
--- portifolio/src/components/NavBar/index.tsx
+++ portifolio/src/components/NavBar/index.tsx
@@ -1,52 +1,23 @@
 import Link from 'next/link';
 import style from './style.min.module.css';
-import { BiSolidCameraMovie } from 'react-icons/bi';
-import Lupa from '../buttons/lupa';
-import ListArrow from '../buttons/listArrow';
 
 const NavBar = () => {
   return (
     <nav className={style.navbar}>
       <div id={style.logo}>
-        <BiSolidCameraMovie />
-        <h2 className={style.logo_title}>Cinemania</h2>
+        <h2 className={style.logo_title}>Portfolio</h2>
         <ul id={style.menu}>
           <li>
-            <Link href={'/'}>Inicio</Link>
+            <Link href={'/'}>Sobre</Link>
           </li>
           <li>
-            <Link href={'/'}>Filmes</Link>
+            <Link href={'/'}>Skill</Link>
           </li>
-          <li className={style.block}>
-            <Link href={'/'}>Series</Link>
-          </li>
-          <li className={style.genero_list}>
-            <div id={style.arrow}>
-              <ListArrow
-                text="Gênero "
-                elementId="#generos"
-                classValue="style_min_visibl__z0LRL"
-              />
-            </div>
-            <div id="generos" className={style.generos}>
-              <div className={style.selected}>teste sdsf</div>
-              <div>teste sdsf</div>
-              <div>teste sdsf</div>
-              <div>teste sdsf</div>
-              <div>teste sdsf</div>
-              <div>teste sdsf</div>
-              <div>teste sdsf</div>
-              <div>teste sdsf</div>
-              <div>teste sdsf</div>
-              <div>teste sdsf</div>
-            </div>
+          <li>
+            <Link href={'/'}>Projetos</Link>
           </li>
         </ul>
       </div>
-      <form id={style.search}>
-        <Lupa text="" elementId="#lupa" classValue="style_min_active__RJ0Ed" />
-        <input id="lupa" className="" type="text" placeholder="Buscar ..." />
-      </form>
     </nav>
   );
 };
